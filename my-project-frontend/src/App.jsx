033b9f714b6be0a629315fc94a440a506e7c44e0
JerinJohn30 <jerinjohn3010@gmail.com>
2025-01-25 17:27:03 +0530

    Frontend skeleton created

---
+++ my-project-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h3>App Component</h3>
+    </>
+  )
+}
+
+export default App
